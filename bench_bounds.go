e646e452f96c61d6886eda1b8aa5c04c40110969
red_byte <developer.sokolov@gmail.com>
2021-04-06 13:21:44 +0300

    @feat: new benchmarks - check bounds operations

---
+++ bench_bounds.go
@@ -0,0 +1,40 @@
+package go_benchmark
+
+import "testing"
+
+//Use flag -gcflags=-S to disassemble Benchmark.
+var v = make([]int, 9)
+
+var A, B, C, D, E, F, G, H, I int
+
+func BenchmarkBoundsCheckInOrder(b *testing.B) {
+	var a, _b, c, d, e, f, g, h, i int
+	for n := 0; n < b.N; n++ {
+		a = v[0]
+		_b = v[1]
+		c = v[2]
+		d = v[3]
+		e = v[4]
+		f = v[5]
+		g = v[6]
+		h = v[7]
+		i = v[8]
+	}
+	A, B, C, D, E, F, G, H, I = a, _b, c, d, e, f, g, h, i
+}
+
+func BenchmarkBoundsCheckOutOfOrder(b *testing.B) {
+	var a, _b, c, d, e, f, g, h, i int
+	for n := 0; n < b.N; n++ {
+		i = v[8]
+		a = v[0]
+		_b = v[1]
+		c = v[2]
+		d = v[3]
+		e = v[4]
+		f = v[5]
+		g = v[6]
+		h = v[7]
+	}
+	A, B, C, D, E, F, G, H, I = a, _b, c, d, e, f, g, h, i
+}
